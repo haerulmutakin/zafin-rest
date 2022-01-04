beffe6dbf9c97aab8c7d763350684dd0b9fd1b96
haerulmutakin <haerulm.takin@gmail.com>
2022-01-04 14:02:10 +0700

    enable cors

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 const port = process.env.PORT || 5000;
 import appRoutes from './src/routes/index.js';
 
 const app = express();
+
+var corsOptions = {
+    origin: '*'
+  }
+app.use(cors(corsOptions));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/v1/api/user', appRoutes);
