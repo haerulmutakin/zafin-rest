d065cc2daf375b15c0ed281ceee91f265a8cb6b6
haerulmutakin <haerulm.takin@gmail.com>
2022-01-04 12:06:48 +0700

    change port

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import express from 'express';
-import { PORT } from './config/config';
 import appRoutes from './routes';
-require('dotenv/config');
 
 const app = express();
 app.use('/v1/api', appRoutes);
 
 
-app.listen(process.env.PORT, () => console.log('application running on port', PORT));
+app.listen(process.env.PORT, () => console.log('application running on port', process.env.PORT));
